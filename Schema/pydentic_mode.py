80a70ab56f4dd2f51ca50acf7bb1a24acba47818
Ravi Der <derravi00@gmail.com>
2026-06-23 18:33:15 +0530

    Add Multiple model and also add Recommendation systme

--- Schema/pydentic_mode.py
+++ Schema/pydentic_mode.py
@@ -3,11 +3,8 @@ from typing import Annotated
 
 class UserInput(BaseModel):
 
-    Age:Annotated[float,Field(...,description="Enter the Age:",examples=[21])]
-    Attendance:Annotated[float,Field(...,description="Enter the Attendance:",examples=[80])]
     StudyHours:Annotated[float,Field(...,description="Enter the StudyHours:",examples=[40])]
     PreviousMarks:Annotated[float,Field(...,description="Enter the PreviousMarks:",examples=[21])]
-    SleepHours:Annotated[float,Field(...,description="Enter the SleepHours:",examples=[8])]
     Semester1:Annotated[float,Field(...,description="Enter the Semester1:",examples=[84])]
     Semester2:Annotated[float,Field(...,description="Enter the Semester2:",examples=[74])]
     Semester3:Annotated[float,Field(...,description="Enter the Semester3:",examples=[69])]
--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from fastapi.responses import JSONResponse
 app = FastAPI(title="Student Exam Score Prediction using Linear Regression")
 
 with open('models/model.pkl', 'rb') as f:
-    model = pickle.load(f)
+    loaded_data = pickle.load(f)
+
+rnd = loaded_data['Random_forest']
 
 @app.get("/")
 def default():
@@ -17,18 +19,16 @@ def default():
 def predict_output(predict:UserInput):
 
     df = pd.DataFrame([{
-        'Age':predict.Age,
-        'Attendance':predict.Attendance,
+
         'StudyHours':predict.StudyHours,
         'PreviousMarks':predict.PreviousMarks,
-        'SleepHours':predict.SleepHours,
         'Semester1':predict.Semester1,
         'Semester2':predict.Semester2,    
         'Semester3':predict.Semester3,
         'Semester4':predict.Semester3
     }])
 
-    prediction_output = round(float((model.predict(df))[0][0]),2)
+    prediction_output = round(float((rnd.predict(df))[0]),2)
 
     return JSONResponse(status_code=200,content={
         "The Total Predicted Marks of the student is:":prediction_output
