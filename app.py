380af07ab284e8cf406a3eff46b23addd25de587
Ravi Der <derravi00@gmail.com>
2026-06-24 09:46:16 +0530

    Add pickle model and take the perfect output from the fast apis

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = FastAPI(title="Student Exam Score Prediction using Linear Regression")
 with open('models/model.pkl', 'rb') as f:
     loaded_data = pickle.load(f)
 
-rnd = loaded_data['Random_forest']
+best_mdl = loaded_data['bst_model']
+best_accuracy = loaded_data['bst_accuracy']
 
 @app.get("/")
 def default():
@@ -28,8 +29,9 @@ def predict_output(predict:UserInput):
         'Semester4':predict.Semester3
     }])
 
-    prediction_output = round(float((rnd.predict(df))[0]),2)
+    prediction_output = round(float((best_mdl.predict(df))[0]),2)
 
     return JSONResponse(status_code=200,content={
-        "The Total Predicted Marks of the student is:":prediction_output
+        "The Total Predicted Marks of the student is:":prediction_output,
+        "With the Accuracy of the model is " : f"{round(best_accuracy,2)*100}%"
     })
